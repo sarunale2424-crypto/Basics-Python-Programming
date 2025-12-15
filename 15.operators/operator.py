2f1a001723309dde2dbe987947d3e0554c19fe40
Sarun <sarun.ale2424@gmai.com>
2025-12-15 23:32:44 +0900

    15.operators and types of operator and arithmetic operator

---
+++ 15.operators/operator.py
@@ -0,0 +1,58 @@
+#operator: An operator is a symbol that tells the computer to perform certain operation/calculation
+#types of operator in Python
+#1.Arithmetic operator
+#2.Relational/compasison operator
+#3.Logical operator
+#4.Bitwise Operator
+#5.Assignment operator 
+#6.Special operator
+
+#1.Arithmetic Operator
+# + => addition operator
+# - =>subtraction operator
+# * => multiplication operator
+# / =>division operator(float)
+# % =>Modulo or Remainder operator
+# // => Floor Division or Integer Division operator
+# ** => Exponent or power operator
+
+# num1 = 20
+# num2 = 3.0
+
+# print("num1+num2=",num1+num2) #num1+num2=23
+# print("num1-num2=",num1-num2) #num1-num2=17
+# print("num1 * num2=",num1*num2) #num1*num2=60
+# print("num1/num2=",num1/num2) #num1/num2=6.6666666
+# print("num1 % num2",num1 % num2) #num1%num2=2
+# print("num1 // num2",num1 // num2) #num1//num2=6
+# print("num1 ** num2",num1 ** num2) #num1**num2=8000
+
+# # + => It is also used to concatenate string
+# first_name = "john"
+# last_name = "doe"
+# full_name = first_name +" " +last_name # john doe
+# print(full_name)
+
+
+# print('hello'+str(3)) #python is tightly coupled dynamic typed programming language
+
+
+# * => It is also used to repeat string with fixed number of time
+
+# name = "*"
+
+# number = 10
+
+# result = name*number
+# print(result)
+
+x = 100
+
+# print(x/0) #ZeroDivisionError: division by zero
+# print(x%0)  #ZeroDivisionError: integer modulo by zero
+# print(x//0) #ZeroDivisionError: integer division or modulo by zero
+ 
+
+
+
+
